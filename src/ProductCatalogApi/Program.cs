0f3eb0e0eb5d6fe279eb533c0fa990d2221091be
voommen-livefront <voommen@livefront.com>
2025-04-23 09:44:00 -0500

    Scalar instead of Swagger

--- src/ProductCatalogApi/Program.cs
+++ src/ProductCatalogApi/Program.cs
@@ -2,6 +2,7 @@ using ProductCatalogApi;
 using ProductCatalogApi.DataAccess;
 using Microsoft.EntityFrameworkCore;
 using ProductCatalogApi.Services;
+using Scalar.AspNetCore;
 internal class Program
 {
     private static void Main(string[] args)
@@ -28,15 +29,21 @@ internal class Program
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
         builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddSwaggerGen();
+        // builder.Services.AddSwaggerGen();
+
+        builder.Services.AddOpenApi();
 
         var app = builder.Build();
 
         // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            // Use scalar instead of swagger
+            // app.UseSwagger();
+            // app.UseSwaggerUI();
+
+            app.MapOpenApi();
+            app.MapScalarApiReference();
         }
 
         app.UseHttpsRedirection();
